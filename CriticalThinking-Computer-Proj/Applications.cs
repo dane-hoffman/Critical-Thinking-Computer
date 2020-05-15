b75cbccc18b55e664b9d99106b0d523ca203edbf
unknown <dane.hoffman90@gmail.com>
2020-05-15 13:16:55 -0600

    Created Applications class, added member variables, and constructor with variables assigned as conditions within class constructor.

---
+++ CriticalThinking-Computer-Proj/Applications.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinking_Computer_Proj
+{
+    class Applications
+    {
+        public string applicationName;
+        public string applicationType;
+        public double requiredRAM;
+        public double requiredStorage;
+
+
+        public Applications()
+        {
+            applicationName = "Slack";
+            applicationType = "Messagin service";
+            requiredRAM = 4.0; //In gigabytes
+            requiredStorage = 0.512; //In gigabytes
+
+
+
+        }
+
+
+
+    }
+}
