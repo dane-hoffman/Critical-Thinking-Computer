0170e52a50432dde92d91f0aa7d77747dd329941
unknown <dane.hoffman90@gmail.com>
2020-05-18 09:08:04 -0600

    Added 'TextEditor' and 'Games' class as the ChildClass to 'Applications' Parent/SuperClass.

---
+++ CriticalThinking-Computer-Proj/Games.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinking_Computer_Proj
+{
+    public class Games : Applications
+    {
+        public double requiredEffectiveMemory;
+
+
+        public Games()
+        { 
+        
+        }
+
+
+
+    }
+}
---
+++ CriticalThinking-Computer-Proj/TextEditor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinking_Computer_Proj
+{
+    public class TextEditor : Applications
+    {
+
+
+    }
+}
